ec236bb2a5777a02a709f5f86f5e3d88d3c3fb23
Nils-Levandovskis <65115709+Nils-Levandovskis@users.noreply.github.com>
2023-08-15 00:19:55 +0300

    Update tableTest.py

--- tableTest.py
+++ tableTest.py
@@ -1,7 +1,17 @@
+import os
+
+
+  
 def main():
   print("| First | Second |")
   print("| --- | --- |")
   print("| 1 | 2 |")
+
+  env_file = os.getenv('GITHUB_ENV')
+
+  with open(env_file, "a") as myfile:
+    myfile.write("TABLE_VAR=| First | Second |\n| --- | --- |\n| 1 | 2 |")
+  
   return 0
 if __name__ == "__main__":
     main()
